bc0e299dc2e0ebf6497c5fab1ab25b52389c2351
Gary Bake <gary.bake@planixs.com>
2016-12-19 13:06:00 +0000

    Added rotor change state change model

--- machine/rotor.py
+++ machine/rotor.py
@@ -4,12 +4,16 @@ import map_set
 
 class Rotor:
 
-    def __init__(self, key_order, start_state):
+    def __init__(self, key_order, start_state=None):
         """
         state is which character points to the first of the key order
         """
         self.identity_map = map_set.IDENTITY_MAP
         self.key_order = key_order
+
+        if not start_state:
+            start_state = self.identity_map[0]
+
         self.state = start_state
 
 
@@ -25,9 +29,12 @@ class Rotor:
         Use reverse=True for a reverse mapping
         """
         if not reverse:
-            idx = self.identity_map.index(letter)
+            idx = self.identity_map.index(letter) - self.state_index()
+            if idx < 0:
+                idx = idx + 26
             return self.key_order[idx]
         else:
-            idx = self.key_order.index(letter)
+            idx = self.key_order.index(letter) + self.state_index()
+            if idx > 25:
+                idx = idx - 26
             return self.identity_map[idx]
-
--- machine/test_rotor.py
+++ machine/test_rotor.py
@@ -8,34 +8,65 @@ import map_set
 class RotorTest(unittest.TestCase):
 
     def setUp(self):
-        mapping = 'BCADEFGHIJKLMNOPQRSTUVWXYZ'
-        r = Rotor(key_order=mapping, start_state='A')
+        # mappi = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        self.mapping = 'WXYZABCDEFGHIJKLMNOPQRSTUV'
+        r = Rotor(key_order=self.mapping, start_state='A')
         self.rtr = r
 
     def test_start_position(self):
         r = self.rtr
         self.assertEqual(r.state, 'A')
-        self.assertEqual(r.key_order[0], 'B')
-        self.assertEqual(r.key_order[3], 'D')
+        self.assertEqual(r.key_order[0], 'W')
+        self.assertEqual(r.key_order[3], 'Z')
 
+        r2 = Rotor(key_order=self.mapping)
+        self.assertEqual(r2.state_index(), 0)
+
+        r2 = Rotor(key_order=self.mapping, start_state='E')
+        self.assertEqual(r2.state_index(), 4)
+
+    def test_letter_mapping(self):
+        r = self.rtr
+        self.assertEqual(r.map_letter('A'), 'W')
+        self.assertEqual(r.map_letter('D'), 'Z')
+        self.assertEqual(r.map_letter('Z'), 'V')
+
+        self.assertEqual(r.map_letter('A', reverse=True), 'E')
+        self.assertEqual(r.map_letter('D', reverse=True), 'H')
+        self.assertEqual(r.map_letter('Z', reverse=True), 'D')
+        self.assertEqual(r.map_letter('W', reverse=True), 'A')
 
     def test_state(self):
         r = self.rtr
         self.assertEqual(r.state_index(), 0)
 
         r.set_state('D')
+        # mapping_i = ABCDEFGHIJKLMNOPQRSTUVWXYZABC'
+        # mapping_m = TUVWXYZABCDEFGHIJKLMNOPQRSTUV'
         self.assertEqual(r.state, 'D')
         self.assertEqual(r.state_index(), 3)
 
-    def test_letter_mapping(self):
-        r = self.rtr
-        self.assertEqual(r.map_letter('A'), 'B')
-        self.assertEqual(r.map_letter('D'), 'D')
-        self.assertEqual(r.map_letter('Z'), 'Z')
+        self.assertEqual(r.map_letter('G'), 'Z')
+        self.assertEqual(r.map_letter('P'), 'I')
+        self.assertEqual(r.map_letter('C'), 'V')
 
-        self.assertEqual(r.map_letter('B', reverse=True), 'A')
-        self.assertEqual(r.map_letter('D', reverse=True), 'D')
-        self.assertEqual(r.map_letter('Z', reverse=True), 'Z')
+        self.assertEqual(r.map_letter('A'), 'T')
+        self.assertEqual(r.map_letter('Z'), 'S')
+
+        self.assertEqual(r.map_letter('T', reverse=True), 'A')
+        self.assertEqual(r.map_letter('H', reverse=True), 'O')
+        self.assertEqual(r.map_letter('S', reverse=True), 'Z')
+
+        r.set_state('Z')
+        # mapping_i = ZABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        # mapping_m = WXYZABCDEFGHIJKLMNOPQRSTUVW'
+        self.assertEqual(r.state, 'Z')
+        self.assertEqual(r.state_index(), 25)
+
+        self.assertEqual(r.map_letter('A'), 'X')
+        self.assertEqual(r.map_letter('M'), 'J')
+        self.assertEqual(r.map_letter('Z'), 'W')
+
 
 if __name__ == '__main__':
     unittest.main()
